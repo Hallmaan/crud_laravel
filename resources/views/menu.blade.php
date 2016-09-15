23719d1cd93023cf56b9eaddfbeb126518a589bb
mgoid <mgo.id@outlook.com>
2016-09-15 13:09:57 +0700

    laravel_crud

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,7 @@
+
+        <li><a href="{{url('/')}}">Home</a></li>
+        <li><a href="{{url('/blog')}}">Blog</a></li>
+        <li><a href="{{url('/about')}}">About</a></li>
+        <li><a href="{{url('/contact-us')}}">Contact us</a></li>
+        <li><a href="{{url('/auth/register')}}">Register</a></li>
+        <li><a href="{{url('/auth/login')}}">Login</a></li>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('header')
+
+    <title>laravel &raquo; home</title>
+
+@endsection
+
+@section('content')
+
+    <h1>hallo cuk</h1>
+
+@endsection
